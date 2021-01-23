d520dc46fa6b9b191b4000be2088dedf852849c4
ByFow <39157740+ByFow@users.noreply.github.com>
2021-01-23 19:59:30 +0100

    Add Reviews

---
+++ lang/en/public/reviews.php
@@ -0,0 +1,22 @@
+<?php
+
+    $R_BUTTON_REVIEW = "Send a review :D";
+	$R_BUTTON_REVIEW_ALREADY = "Vous avez déjà donné votre avis pour ce serveur ;) !";
+	$R_BUTTON_REVIEW_OFF = "Ce serveur n'a pas activé la fonctionnalité... :'(";
+    $R_NOTE = "Note";
+
+    $R_REVIEW_NO_TEXT = "Cette avis n'a pas de texte.. Surement car ce serveur est indescriptible.. !";
+
+    // La partie avis :)
+	// Les avis ne sont pas traduit car ils sont dans la langue d'origine du serveur, en toute logique évidemment..
+	$R_REVIEW_NO_TITLE = "Désolé, ce serveur n'a aucun avis...";
+	$R_REVIEW_NO_CONTENT = "Si tu es connecté, tu peux poster un avis.";
+	$R_REVIEW_NO_BUTTON = "Envoyer un avis";
+
+	// Le modal dans lequel vous envoyez votre avis !
+	$R_MODAL_REVIEW_TITLE = "Envoyer un avis !";
+	$R_MODAL_REVIEW_NOTATION = "Note";
+	$R_MODAL_REVIEW_COMMENT = "Commentaire";
+	$R_MODAL_REVIEW_SEND = "Envoyer";
+
+?>
